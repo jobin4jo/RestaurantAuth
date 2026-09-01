8b66aca404bfed593fc829aaf578884a947c90c5
Jobin s <jobins802@gmail.com>
2026-09-01 22:17:51 +0530

    Add EF Core with SQL Server and User entity support
    Integrated EF Core into RestaurantAuth with SQL Server configuration and DI setup. Added User entity and RestaurantDbContext for Users management. Included initial migration for Users table. Updated appsettings.json with connection string and project files with EF Core dependencies.

---
+++ RestaurantAuth.Infrastructure/RestaurantDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAuth.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAuth.Infrastructure
+{
+    public class RestaurantDbContext:DbContext
+    {
+        public RestaurantDbContext(
+        DbContextOptions<RestaurantDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
